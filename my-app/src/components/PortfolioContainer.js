7524dbc27f86171404a42e9594f4d3b21be52943
unknown <joseph.harley02@gmail.com>
2023-03-05 18:20:43 -0500

    made navbar and set up pages to display when section on navbar is clicked

--- my-app/src/components/PortfolioContainer.js
+++ my-app/src/components/PortfolioContainer.js
@@ -1,5 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
-export default function PortfolioContainer() {
+function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState("About");
+
+    const renderPage = () => {
+        if (currentPage === "About") {
+            return <About />;
+        }
+        else if (currentPage === "Portfolio") {
+            return <Portfolio />;
+        }
+        else if (currentPage === "Contact") {
+            return <Contact />;
+        }
+        else {
+            return <Resume />;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+    )
 
 }
+
+export default PortfolioContainer;
