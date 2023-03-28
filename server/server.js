cf47a6bdfd6e9e7ed9f1c9e7f455297720c6b934
unknown <joseph.harley02@gmail.com>
2023-03-27 20:34:54 -0400

    created server for deployment

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
+});
