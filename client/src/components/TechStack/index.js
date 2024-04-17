ab8765386715c5b86176782f90bc61fe874c3ce4
unknown <joseph.harley02@gmail.com>
2024-04-17 13:53:01 -0400

    updated tech stack section

--- client/src/components/TechStack/index.js
+++ client/src/components/TechStack/index.js
@@ -1,5 +1,4 @@
-import { FaHtml5, FaCss3, FaJs, FaReact, FaBootstrap } from "react-icons/fa";
-import { SiMongodb, SiMysql, SiExpress, SiNodedotjs, SiTailwindcss, SiGraphql } from "react-icons/si";
+import { SiMongodb, SiPostgresql, SiExpress, SiNodedotjs, SiGo, SiTypescript, SiJavascript, SiDocker, SiReact } from "react-icons/si";
 import { Tooltip } from "react-tippy";
 import "react-tippy/dist/tippy.css";
 
@@ -10,38 +9,32 @@ const TechStack = () => {
           Tech Stack
         </h2>
         <div className="flex flex-wrap justify-center items-center">
-            <Tooltip title="HTML">
-                <FaHtml5 className="text-6xl text-orange-500 mx-6 my-4 hover:text-gray-500" />
-            </Tooltip>
-            <Tooltip title="CSS">
-                <FaCss3 className="text-6xl text-blue-500 mx-6 my-4 hover:text-gray-500" />
+            <Tooltip title="Go Promming Language">
+                <SiGo className="text-6xl text-blue-500 mx-6 my-4 hover:text-gray-500" />
             </Tooltip>
             <Tooltip title="JavaScript">
-                <FaJs className="text-6xl text-yellow-500 mx-6 my-4 hover:text-gray-500" />
+                <SiJavascript className="text-6xl text-yellow-500 mx-6 my-4 hover:text-gray-500" />
             </Tooltip>
-            <Tooltip title="React">
-                <FaReact className="text-6xl text-blue-400 mx-6 my-4 hover:text-gray-500" />
+            <Tooltip title="TypeScript">
+                <SiTypescript className="text-6xl text-blue-500 mx-6 my-4 hover:text-gray-500" />
+            </Tooltip>
+            <Tooltip title="Docker">
+                <SiDocker className="text-6xl text-blue-600 mx-6 my-4 hover:text-gray-500" />
+            </Tooltip>
+            <Tooltip title="PostgreSQL">
+                <SiPostgresql className="text-6xl text-blue-500 mx-6 my-4 hover:text-gray-500" />
             </Tooltip>
             <Tooltip title="MongoDB">
                 <SiMongodb className="text-6xl text-green-500 mx-6 my-4 hover:text-gray-500" />
             </Tooltip>
-            <Tooltip title="MySQL">
-                <SiMysql className="text-6xl text-blue-900 mx-6 my-4 hover:text-gray-500" />
-            </Tooltip>
             <Tooltip title="Express">
                 <SiExpress className="text-6xl text-black mx-6 my-4 hover:text-gray-500" />
-            </Tooltip>
-            <Tooltip title="GraphQL">
-                <SiGraphql className="text-6xl text-pink-500 mx-6 my-4 hover:text-gray-500" />
             </Tooltip>
             <Tooltip title="Node.js">
                 <SiNodedotjs className="text-6xl text-green-600 mx-6 my-4 hover:text-gray-500" />
             </Tooltip>
-            <Tooltip title="Tailwind CSS">
-                <SiTailwindcss className="text-6xl text-blue-400 mx-6 my-4 hover:text-gray-500" />
-            </Tooltip>
-            <Tooltip title="Bootstrap">
-                <FaBootstrap className="text-6xl text-purple-600 mx-6 my-4 hover:text-gray-500" />
+            <Tooltip title="React">
+                <SiReact className="text-6xl text-blue-400 mx-6 my-4 hover:text-gray-500" />
             </Tooltip>
         </div>
     </section>
