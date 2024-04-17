8c0729570ef4d9fbb957e214b76a2bd4802d94b0
unknown <joseph.harley02@gmail.com>
2024-04-17 14:10:55 -0400

    updated portfolio section

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -7,33 +7,20 @@ const Portfolio = () => {
         </h1>
         {/* Full Plate project */}
         <div className="mb-12">
-          <h1 className="text-2xl lg:text-4xl font-bold mb-4">
-            Full Plate
-          </h1>
-          <div className="flex flex-wrap mb-4">
-            <div className="w-full md:w-1/2 md:pr-4">
-              <img
-                className="h-full w-full object-cover"
-                src="./images/fullplate.png"
-                alt="Project Screenshot"
-              />
-            </div>
+          <h2 className="text-2xl text-center lg:text-3xl font-bold mb-4">
+            Gymplanr
+          </h2>
+          <div className="flex flex-wrap mb-4 justify-center">
             <div className="w-full md:w-1/2">
               <p className="text-md lg:text-lg mb-4">
-              Full Plate is a recipe-sharing website that I worked on as part of a group project with 3 other developers in a week. It features recipe uploads and viewing. I gained valuable experience in full-stack development and project management through this project
+              A lot of people want to get into fitness, but they aren't sure where to start. I know when I first started my fitness journey there was so much information I had to learn. How to structure a workout, what exercises hit which muscles, how many sets and reps do I do. The list goes on and on, so I built gymplanr to streamline the process to generate a full workout plan based on what you're looking for with no prior knowledge about fitness.
               </p>
               <p className="text-md lg:text-lg mb-4">
-                Technologies Used: React, GraphQL, MongoDB, Express, Node
+                Technologies Used: Go, PostgreSQL, Cobra, Bcrypt
               </p>
-              <div className="flex mb-4">
+              <div className="flex mb-4 justify-center">
                 <a
-                  href="https://secure-dawn-49208.herokuapp.com/"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
-                >
-                  Visit Website
-                </a>
-                <a
-                  href="https://github.com/Bmoore4452/Full-Plate"
+                  href="https://github.com/harljos/gymplanr"
                   className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
                 >
                   GitHub Repository
